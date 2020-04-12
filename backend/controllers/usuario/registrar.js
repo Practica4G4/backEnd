e3d64728f7cf437feab6de4df0ac53c5116561a5
PabloOsuna1997 <2472932372001@ingenieria.usac.edu.gt>
2020-04-12 12:35:03 -0600

    feat: validacion de datos

--- backend/controllers/usuario/registrar.js
+++ backend/controllers/usuario/registrar.js
@@ -2,12 +2,26 @@ const db = require('../../src/dataBase/conexion');
 module.exports = function (router) {
 
     router.get('/', async(req, res) => {
-        const  entradaRegistro = require('../../src/mapeoObjetos/usuario/entradaRegistro');
+       try{
+           const  entradaRegistro = require('../../src/mapeoObjetos/usuario/entradaRegistro');
         
-        //insercion a la base de datos
-        const result = await db.query('INSERT INTO USUARIO set ?',[entradaRegistro(req.body).data]);
+            //insercion a la base de datos
+            const result = await db.query('INSERT INTO USUARIO set ?',[entradaRegistro(req.body).data]);
 
-        console.log(result);
-        res.status(result.serverStatus).send({ mensaje: result.message });       
+            console.log(result);
+            if(result.affectedRows == 1){
+                res.status(200).send({ mensaje: "Usuario Registrado" });     
+            }else{
+
+            } 
+        }catch(error){
+            if(error.code == 'ER_DUP_ENTRY'){
+                res.message = error;
+                res.status(400).send({ mensaje: 'El dpi o el numero de cuenta ya se encuentran en uso.' }); 
+            }else{
+                res.message = error;
+                res.status(500).send({ mensaje: 'No se pudo completar la solicitud' });
+            }
+        }
     });
 };
