8329152398b18f45cef27bd2e6e43d9ad9dffa3b
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-04-29 21:20:18 +0000

    feat: Se agrego Prueba Unitaria al login

--- backend/src/data/usuario/autenticar.js
+++ backend/src/data/usuario/autenticar.js
@@ -1,9 +1,9 @@
 const db = require('../../dataBase/conexion');
 const entradaAutenticar = require('../../../src/mapeoObjetos/usuario/entradaAutenticar');
 
-module.exports.autenticar = async (req) => {
+module.exports.autenticar = async (params) => {
     try {
-        const data = entradaAutenticar(req.body).data;
+        const { data } = entradaAutenticar(params);
         const result = await db.query('SELECT * FROM USUARIO WHERE noCuenta = ? AND contrasena = ?;', [data.noCuenta, data.contrasena]);
         if (result.length > 0) {
             return result;
