490a175b699b49391a8d3f01f37cad3217cb07e6
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-04-29 02:01:28 +0000

    feat: Cambiar estructura registrar usuario a funcion

--- backend/controllers/usuario/registrar.js
+++ backend/controllers/usuario/registrar.js
@@ -1,27 +1,17 @@
 const db = require('../../src/dataBase/conexion');
+const  { registrar } = require('../../src/data/usuario/registrar');
+        
 module.exports = function (router) {
-
     router.post('/', async(req, res) => {
        try{
-           const  entradaRegistro = require('../../src/mapeoObjetos/usuario/entradaRegistro');
-        
-            //insercion a la base de datos
-            const result = await db.query('INSERT INTO USUARIO set ?',[entradaRegistro(req.body).data]);
-
-            console.log(result);
-            if(result.affectedRows == 1){
+           const result = await registrar(req);           
+            if(result){
                 res.status(200).send({ mensaje: "Usuario Registrado" });     
             }else{
-
+                res.status(400).send({ mensaje: "No se pudo registrar al usuario." });   
             } 
         }catch(error){
-            if(error.code == 'ER_DUP_ENTRY'){
-                res.message = error;
-                res.status(400).send({ mensaje: 'El dpi o el numero de cuenta ya se encuentran en uso.' }); 
-            }else{
-                res.message = error;
-                res.status(500).send({ mensaje: 'No se pudo completar la solicitud' });
-            }
+            res.status(500).send({ mensaje: "No se pudo completar la solicitud." });   
         }
     });
 };
