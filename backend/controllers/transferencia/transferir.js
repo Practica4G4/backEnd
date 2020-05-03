288485a5f40b3c6d3bb6e836efd9512ea1f0920a
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-05-02 19:50:42 -0600

    feat: Prueba practica 4.

--- backend/controllers/transferencia/transferir.js
+++ backend/controllers/transferencia/transferir.js
@@ -1,31 +1,39 @@
 const db = require('../../src/dataBase/conexion');
-const  {saldoOrigen}  = require('../../src/data/transferencias/saldoOrigen');
-const  {cuentaDestino}  = require('../../src/data/transferencias/cuentaDestino');
-const  {transferir} = require('../../src/data/transferencias/transferir');
+const { saldoOrigen } = require('../../src/data/transferencias/saldoOrigen');
+const { cuentaDestino } = require('../../src/data/transferencias/cuentaDestino');
+const { transferir } = require('../../src/data/transferencias/transferir');
+
+// const  {saldoOrigen}  = require('../../src/data/transferencias/saldoOrigen');
+// const  {cuentaDestino}  = require('../../src/data/transferencias/cuentaDestino');
+// const  {transferir} = require('../../src/data/transferencias/transferir');
+
 module.exports = function (router) {
 
-    router.post('/', async(req, res) => {
-       try{           
-            const existeCuentaDestino=await cuentaDestino(req.body);
-            if(existeCuentaDestino){
+    router.post('/', async (req, res) => {
+        try {
+            const existeCuentaDestino = await cuentaDestino(req.body);
+            if (existeCuentaDestino) {
                 const saldoSuficiente = await saldoOrigen(req.body);
-                if(saldoSuficiente){
+                if (saldoSuficiente) {
                     const realizarTransferencia = await transferir(req.body);
-                    if(realizarTransferencia){
+                    if (realizarTransferencia) {
                         res.status(200).send({ mensaje: "Transferencia realizada" });
-                    }else{
+                    } else {
                         res.status(400).send({ mensaje: "Transferencia no realizada." });
-                    }                   
-                }else{
+                    }
+                    if (realizarTransferencia) {
+                        res.message = { mensaje: "Transferencia realizada" };
+                    }
+                } else {
                     res.status(400).send({ mensaje: "Transferencia no realizada, fondos no suficientes." });
                 }
-            }else{
+            } else {
                 res.status(400).send({ mensaje: "Transferencia no realizada, no existe la cuenta destino." });
             }
-                        
-        }catch(error){
-                res.message = error;
-                res.status(500).send({ mensaje: 'No se pudo completar la solicitud' });           
+
+        } catch (error) {
+            res.message = error;
+            res.status(500).send({ mensaje: 'No se pudo completar la solicitud' });
         }
     });
 };
